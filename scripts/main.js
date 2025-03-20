c1756ed8847e5b73c278c49c22fed0543f73da26
lalathehung <1808arthur@gmail.com>
2025-03-20 00:13:37 -0300

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamButton = document.querySelector("#menu");
-    const navigation = document.querySelector("nav");
+    const navigation = document.querySelector("nav ul");
+
     hamButton.addEventListener("click", () => {
         navigation.classList.toggle("open");
         hamButton.classList.toggle("open");
     });
 
-    // footer
-    document.getElementById("currentyear").textContent =
-        new Date().getFullYear();
-    document.getElementById(
-        "lastModified"
-    ).textContent = `Last Modified: ${document.lastModified}`;
+    // Set Active Class on Navigation Links
+    document.querySelectorAll("nav a").forEach(link => {
+        link.addEventListener("click", function() {
+            document.querySelector("nav .active").classList.remove("active");
+            this.classList.add("active");
+        });
+    });
 
+    // footer
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = `Last Updated: ${document.lastModified}`;
 });
