f3d8ae1b8457a565f6c5e095d16c209ebf5515ea
lalathehung <1808arthur@gmail.com>
2025-03-21 00:47:02 -0300

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamButton = document.querySelector("#menu");
-    const navigation = document.querySelector("nav ul");
+    const navigation = document.querySelector("nav");
 
     hamButton.addEventListener("click", () => {
         navigation.classList.toggle("open");
         hamButton.classList.toggle("open");
-    });
-
-    // Wayfinding
-    const currentPage = window.location.pathname;
-    const navLinks = document.querySelectorAll("nav a");
-
-    navLinks.forEach(link => {
-        const linkPath = link.getAttribute("href");
-        if (linkPath.startsWith("http")) {
-            return;
-        }
-        const normalizedLinkPath = `./${linkPath}`.replace("././", "./");
-        const normalizedCurrentPage = currentPage.replace("/wdd231/", "/");
-
-        if (normalizedCurrentPage === normalizedLinkPath || 
-            (normalizedCurrentPage === "/index.html" && normalizedLinkPath === "./index.html")) {
-            link.classList.add("active");
-        } else {
-            link.classList.remove("active");
-        }
     });
 
     // Footer updates
